5157682f4324c97e13b330321fbed742c4b5824e
sheena <syeongsn@gmail.com>
2025-09-30 21:00:40 +0800

    fix railway

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/chase/",
+  base: "/",
   build: {
     outDir: "dist",
   },
+  server: {
+    host: '0.0.0.0', // Add this
+    port: process.env.PORT || 5173 // Add this
+  },
+  preview: {
+    host: '0.0.0.0', // Add this
+    port: process.env.PORT || 4173 // Add this
+  }
 });
