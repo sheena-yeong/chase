6f46a5e785539beb2d92bd54b425564dfa207f7d
sheena <syeongsn@gmail.com>
2025-10-01 21:49:48 +0800

    rebuild with HashRouter

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
   build: {
-    outDir: "dist",
+    outDir: "../docs",
+    emptyOutdir: true
   },
   server: {
     host: '0.0.0.0', // Add this
